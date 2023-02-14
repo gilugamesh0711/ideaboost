1a873de6fa1997ee48247b45dd229a25536438f6
backet <hayafune11@gmail.com>
2023-02-14 13:54:44 +0900

    add sign out

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import '@aws-amplify/ui-react/styles.css';
-import { Amplify } from 'aws-amplify';
+import { Amplify,Auth } from 'aws-amplify';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import aws_exports from './aws-exports';
 
@@ -11,18 +11,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Sample React</h1>
+        <h2><a className='App-link' href='.'
+        onClick={Auth.signOut}>
+        Sign Out
+        </a></h2>
       </header>
     </div>
   );
